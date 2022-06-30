9980a9c82432b2d40c09b2865a10063dbe4d6fbb
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-30 18:04:58 +0200

    milestone 2 da completare

--- js/script.js
+++ js/script.js
@@ -10,16 +10,16 @@ const cells = 10;
 const rowCells = rows * cells ;
 
 
-buttonPlay.addEventListener(`click`, function(){
+ buttonPlay.addEventListener(`click`, function(){
     for(let i=1;i <= rowCells ;i++){
         
-            // creo l elemento div
-        const cell = document.createElement(`div`);
-        cell.className = `cell`;
-        
+        // creo l elemento div
+    const cell = document.createElement(`div`);
+    cell.className = `cell`;
     
-        // appendo l elemento all interno del suo contenitore
-        grid.appendChild(`div`);
+    
+    // appendo l elemento all interno del suo contenitore
+    grid.append(cell);
     }
-    }
-)
+}
+ )
