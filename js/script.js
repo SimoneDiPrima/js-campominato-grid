67ce60148f56f947b0f15c77307d5186fc3c4349
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-30 18:21:53 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -10,16 +10,18 @@ const cells = 10;
 const rowCells = rows * cells ;
 
 
- buttonPlay.addEventListener(`click`, function(){
+ buttonPlay.addEventListener(`click`, function(event){
     for(let i=1;i <= rowCells ;i++){
         
         // creo l elemento div
     const cell = document.createElement(`div`);
     cell.className = `cell`;
-    
+    cell.innerText = i;
     
     // appendo l elemento all interno del suo contenitore
     grid.append(cell);
     }
 }
  )
+
+//  devo creare un ciclo for che mi crei l elemento numero e me lo appenda consecutivamente dentro ogni cella
