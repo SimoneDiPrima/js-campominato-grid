5356413c8fa1b8e7ac663038d1f5d61d1a2f16a2
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-30 17:49:17 +0200

    modifiche ciclo for

--- js/script.js
+++ js/script.js
@@ -7,16 +7,17 @@ const buttonPlay = document.getElementById(`button-header`);
 
 const rows = 10;
 const cells = 10;
+const rowCells = rows * cells ;
 
 
 buttonPlay.addEventListener(`click`, function(){
-    for(let i=1;i <=rows ;i++){
-        for(let j=1;j <=cells ; i++){
+    for(let i=1;i <= rowCells ;i++){
+        
             // creo l elemento div
         const cell = document.createElement(`div`);
         cell.className = `cell`;
         
-        }
+    
         // appendo l elemento all interno del suo contenitore
         grid.appendChild(`div`);
     }
