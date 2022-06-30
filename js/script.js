bb1b33203d65b97cd99a416cdfdedd8d167b2efb
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-30 21:13:45 +0200

    esercizio completo(da rivedere milestone 2)

--- js/script.js
+++ js/script.js
@@ -2,26 +2,27 @@
 
 
 const grid = document.getElementById(`grid`)
-
 const buttonPlay = document.getElementById(`button-header`);
 
 const rows = 10;
 const cells = 10;
 const rowCells = rows * cells ;
 
-
- buttonPlay.addEventListener(`click`, function(event){
-    for(let i=1;i <= rowCells ;i++){
-        
+ buttonPlay.addEventListener(`click`, function(){
+    
+    for(let i=1;i <= rowCells ;i++){ 
         // creo l elemento div
     const cell = document.createElement(`div`);
     cell.className = `cell`;
+    cell.addEventListener(`click`,function(){
+        this.classList.toggle(`clicked`);
+        console.log(i);
+    })
     cell.innerText = i;
-    
-    // appendo l elemento all interno del suo contenitore
     grid.append(cell);
     }
-}
- )
-
-//  devo creare un ciclo for che mi crei l elemento numero e me lo appenda consecutivamente dentro ogni cella
+        }
+        )
+        
+        
+    
